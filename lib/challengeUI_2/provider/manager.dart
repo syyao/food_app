b15b11ff628ae9db374f5fa63995ec1fac987e32
Chelida <orphelia.domi@gmail.com>
2020-12-09 16:19:37 +0000

    domi
    manager

--- lib/challengeUI_2/provider/manager.dart
+++ lib/challengeUI_2/provider/manager.dart
@@ -4,7 +4,7 @@ import 'package:food_app/challengeUI_2/provider/NotifierList.dart';
 class Management extends InheritedWidget {
   final dataFood = DataFood();
   @override
-  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+  bool updateShouldNotify(InheritedWidget oldWidget) => false;
 
   static Management of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<Management>();
